9b54d45ca51954d85fab14ffc48d8e80e64a1a12
Stefan Wullems <stefanwullems@Stefans-MacBook-Air.local>
2024-03-26 23:36:29 +0100

    bubble sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,12 +11,24 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  let endIndex = nums.length - 2
+  while(endIndex >= 0) {
+    for(let i = 0; i <= endIndex; i++) {
+      const a = nums[i]
+      const b = nums[i + 1]
+      if (a > b) {
+        nums[i] = b
+        nums[i + 1] = a
+      }
+    }
+    endIndex--
+  }
+  return nums
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
